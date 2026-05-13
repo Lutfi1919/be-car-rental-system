45060f120e223dde445a463c5ad6a1604e7db7d0
Lutfi1919 <luthfiamad19@gmail.com>
2026-05-13 09:28:47 +0700

    refactor method createVehicle jadi stock di set default 0 dan data status default unavailable, lalu jumlah vehicle_unit dihitung dan update data stock vehicle dari jumlah hitungan dan status di rubah jadi available bila data hitungan lebih dari 0

--- controllers/vehicle.controller.js
+++ controllers/vehicle.controller.js
@@ -6,41 +6,52 @@ const { response } = require('../helpers/response.formatter');
 module.exports = {
     createVehicle: async (req, res) => {
         try {
-            const { name, type, transmission, stock, price_per_day, description, status } = req.body;
+            const { name, type, transmission, price_per_day, description } = req.body;
 
             const schema = {
                 name: { type: "string", min: 3 },
                 type: { type: "string", enum: ['sedan', 'SUV', 'hatchback', 'coupe', 'sport'] },
                 transmission: { type: "string", enum: ['manual', 'automatic'] },
-                stock: { type: "number", positive: true, integer: true },
+                // stock: { type: "number", positive: true, integer: true },
                 price_per_day: { type: "number", positive: true, integer: true },
                 description: { type: "string" },
-                status: { type: "string", enum: ['available', 'unavailable'] }
             }
 
             const data = {
                 name: name,
                 type: type,
                 transmission: transmission,
-                stock: Number(stock),
+                // stock: Number(stock),
                 price_per_day: Number(price_per_day),
                 description: description,
-                status: status,
             }
 
             const validate = v.validate(data, schema);
             if (validate.length > 0) {
                 return res.status(400).json(response(400, "Validasi Error", validate))
             }
-
+ 
             const vehicle = await Vehicle.create({
                 name: data.name,
                 type: data.type,
                 transmission: data.transmission,
-                stock: data.stock,
+                stock: 0,
                 price_per_day: data.price_per_day,
                 description: data.description,
-                status: data.status,
+                status: 'unavailable'
+            })
+            
+            const { count } = await Vehicle_unit.findAndCountAll({
+                where: {
+                    vehicle_id: vehicle_id
+                }
+            })
+
+            await Vehicle.update({
+                stock: count,
+                status: count > 0 ? 'available' : 'unavailable'
+            }, {
+                where: { id: vehicle_id }
             })
 
             return res.status(201).json(response(201, 'created', vehicle));
